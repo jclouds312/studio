3535a45e2e0a50798a9b7a2f5ddf4b8d0cb14a6d
Johnatan Vallejo Marulanda <johnatanvallejomarulanda@gmail.com>
2025-08-14 19:30:16 +0000

    Correr el capacitor para que  lo tome la app Android y crear la estructu

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.laburoya.app',
+  appName: 'LaburoYA',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
